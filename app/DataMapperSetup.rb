7bf8250f288d9c119c1ced13b70dfc86fd485064
Tim Williamson <timwilliamson1337@gmail.com>
2018-01-10 15:52:04 +0000

    rubbish commit message

--- app/DataMapperSetup.rb
+++ app/DataMapperSetup.rb
@@ -5,7 +5,7 @@ require 'dm-postgres-adapter'
 
 def data_mapper_setup
   ENV['RACK_ENV'] ||= "development"
-  DataMapper.setup(:default, "postgres://localhost/makersbnb_#{ENV['RACK_ENV']}")
+  DataMapper.setup(:default, "postgres://localhost/gameplatform_#{ENV['RACK_ENV']}")
   DataMapper.finalize
   DataMapper.auto_upgrade!
 end
