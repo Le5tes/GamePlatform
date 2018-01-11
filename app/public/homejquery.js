97594356bcea1d76ebeb0ea7ba3c0218247d7cb0
newtdogg <edwardfrancisnewton@live.com>
2018-01-11 11:14:51 +0000

    fixing sign up form

--- app/public/homejquery.js
+++ app/public/homejquery.js
@@ -2,13 +2,13 @@ signUpForm = function(){
   $("#home-box").empty()
   $("#home-box").html(`<div id="signupform">
     <form action= "/users" method = "post">
-      <input type = "text" name = "username" placeholder="username">
-      <input type = "text" name = "first_name" placeholder="first name">
-      <input type = "text" name = "last_name" placeholder="last_name">
-      <input type = "text" name = "email" placeholder="email">
-      <input type = "password" name = "password" placeholder="password">
-      <input type = "password" name = "password_confirm" placeholder="confirm password">
-      <input type = "submit" value = "Sign up">
+      <div><input type = "text" name = "username" placeholder="username"></div>
+      <div><input type = "text" name = "first_name" placeholder="first name"></div>
+      <div><input type = "text" name = "last_name" placeholder="last_name"></div>
+      <div><input type = "text" name = "email" placeholder="email"></div>
+      <div><input type = "password" name = "password" placeholder="password"></div>
+      <div><input type = "password" name = "password_confirm" placeholder="confirm password"></div>
+      <div><input type = "submit" value = "Sign up"></div>
     </form>
   </div>`)
 }
