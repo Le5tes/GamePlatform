de29fc847f2b5518a529df63d1880a67b0acca5a
newtdogg <edwardfrancisnewton@live.com>
2018-01-11 11:24:53 +0000

    return button jquery added

--- app/public/homejquery.js
+++ app/public/homejquery.js
@@ -2,13 +2,29 @@ signUpForm = function(){
   $("#home-box").empty()
   $("#home-box").html(`<div id="signupform">
     <form action= "/users" method = "post">
-      <input type = "text" name = "username" placeholder="username">
-      <input type = "text" name = "first_name" placeholder="first name">
-      <input type = "text" name = "last_name" placeholder="last_name">
-      <input type = "text" name = "email" placeholder="email">
-      <input type = "password" name = "password" placeholder="password">
-      <input type = "password" name = "password_confirm" placeholder="confirm password">
-      <input type = "submit" value = "Sign up">
+      <div><input class="signupform" type = "text" name = "username" placeholder="username"></div>
+      <div><input class="signupform" type = "text" name = "first_name" placeholder="first name"></div>
+      <div><input class="signupform" type = "text" name = "last_name" placeholder="last_name"></div>
+      <div><input class="signupform" type = "text" name = "email" placeholder="email"></div>
+      <div><input class="signupform" type = "password" name = "password" placeholder="password"></div>
+      <div><input class="signupform" type = "password" name = "password_confirm" placeholder="confirm password"></div>
+      <div>
+        <input class="signupform" type = "submit" value = "Sign up">
+        <button onclick="return()">Back</button>
+      </div>
     </form>
+  </div>`)
+}
+
+return = function(){
+  $("#home-box").empty()
+  $("#home-box").html(`<div class="container-flex title">
+    <h1>hello</h1>
+  </div>
+  <div class="home-button1">
+    <button class="home-buttons" name="signin" onclick="signUpForm()">Sign In</button>
+  </div>
+  <div class="home-button2">
+    <button class="home-buttons" name="signin" onclick="signUpForm()">Sign Up</button>
   </div>`)
 }
